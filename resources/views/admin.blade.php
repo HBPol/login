e9b9f0edfe56eb931227ba54bf9dda45353294a0
hbp_01 <hector.bolsa@outlook.com>
2018-08-27 21:02:38 +0100

    Checking if authenticated user is admin in the home view. Created admin view.

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,19 @@
+@extends(‘layouts.app’)
+
+@section(‘content’)
+
+<div class=”row”>
+
+	<div class=”col-md-8col-md-offset-2">
+
+		<div class=”panelpanel-default”>
+
+			<div class=”panel-heading btn-primary”>WELCOME TO ADMIN ROUTE</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,22 @@
 
 @section('content')
 <div class="container">
+	@if(\Session::has('error'))
+		<div class="alert alert-danger">
+		{{\Session::get('error')}}
+		</div>
+	@endif
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
+					<?php if(auth()->user()->isAdmin == 1) {?>
+						<div class="panel-body">
+							<a href="{{url('admin/routes')}}">Admin</a>
+						</div>
+					<?php } else echo '<div class="panel-heading">Normal user</div>';?>
                 <div class="card-body">
+                
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
